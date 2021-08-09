45b61de320ccfb3c5a761f2dc2573c7271baa527
osamehgbe <68432204+osamegbe@users.noreply.github.com>
2021-08-09 22:16:57 +0000

    using an F-string, because to avoid having to use +

--- main.py
+++ main.py
@@ -10,9 +10,22 @@ weight = input("enter your weight in kg:")
 
 bmi = int(weight) / float(height) ** 2
 bmi_as_int = int(bmi)
-print (bmi)
+print (bmi_as_int)
 
 
+
+print(8//2)
+print(round(8 / 3, 2))
+result = 4 / 2
+result /= 2
+print (result)
+
+#Using an f string
+score = 0
+height = 1.8
+isWinning = True
+#f-String
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
 
 
 
